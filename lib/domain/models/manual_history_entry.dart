73de938dba376d27195b9bfbe4f39c862ff12335
Raphaël GAUTHIER <raphaelgauthier@outlook.fr>
2026-04-30 22:07:10 +0200

    refactor(domain): ManualHistoryEntry carries List<TourStopAnimal>

--- lib/domain/models/manual_history_entry.dart
+++ lib/domain/models/manual_history_entry.dart
@@ -1,19 +1,25 @@
+import 'tour_stop_animal.dart';
+
 class ManualHistoryEntry {
   final int id;
   final int clientId;
   final DateTime date;
-  final int small;
-  final int large;
+  final List<TourStopAnimal> animals;
   final String? note;
 
   const ManualHistoryEntry({
     required this.id,
     required this.clientId,
     required this.date,
-    required this.small,
-    required this.large,
+    required this.animals,
     this.note,
   });
 
-  int get total => small + large;
+  int get animalsTotal {
+    var total = 0;
+    for (final a in animals) {
+      total += a.count;
+    }
+    return total;
+  }
 }
